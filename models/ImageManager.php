c145d4f72bb5f65924b25d9deec8b18662d9f619
Kolya Siryk <nikolay.siryk@bigdropinc.com>
2017-12-01 13:13:13 +0200

    update

--- models/ImageManager.php
+++ models/ImageManager.php
@@ -98,6 +98,7 @@ class ImageManager extends ActiveRecord
                 'class' => BlameableBehavior::className(),
                 'createdByAttribute' => 'createdBy',
                 'updatedByAttribute' => 'modifiedBy',
+                'value' => \Yii::$app->user->identity->_id,
             ];
         }
 
